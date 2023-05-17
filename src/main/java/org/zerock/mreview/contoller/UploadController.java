f8eda8cae0ed4d6e137754a64046569ebd49fc75
intteliHome <iseoulyou@gmail.com>
2023-05-17 23:37:56 +0900

    file upload with thumbnail update

--- src/main/java/org/zerock/mreview/contoller/UploadController.java
+++ src/main/java/org/zerock/mreview/contoller/UploadController.java
@@ -1,17 +1,29 @@
 package org.zerock.mreview.contoller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,17 +33,34 @@ public class UploadController {
     @Value("${org.zerock.upload.path}") // application.properties의 변수
     private String uploadPath;
 
-    @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    private String makeFolder(){
 
-        System.out.println("----------------------------");
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        // make folder -----------------
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (uploadPathFolder.exists() == false){
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
+    }
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        log.info("----------uploadAjax-----------");
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile : uploadFiles){
 
             // 이미지 파일만 업로드 가능
             if (uploadFile.getContentType().startsWith("image") == false){
-                log.warn("this is not image type");;
-                return;
+                log.warn("this is not a image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -51,27 +80,57 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
+                // 실제 이미지 저장
                 uploadFile.transferTo(savePath);
+
+                // 썸네일 저장
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator +
+                        "s_" + uuid + "_" + fileName;
+
+                // 썸네일 파일 이름은 중간에 s_로 시작하도록
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                // 썸네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
+                resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
+
             } catch (IOException e) {
-                e.printStackTrace();;
+                e.printStackTrace();
             }
         } // end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     } // uploadAjax end
 
-    private String makeFolder(){
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
 
-        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        ResponseEntity<byte[]> result = null;
 
-        String folderPath = str.replace("/", File.separator);
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
 
-        // make folder -----------------
-        File uploadPathFolder = new File(uploadPath, folderPath);
+            log.info("fileName: " + srcFileName);
 
-        if (uploadPathFolder.exists() == false){
-            uploadPathFolder.mkdirs();
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return folderPath;
+        return result;
     }
+
+
 
 
 }
